bbdf0caa63ab0617d8ba024c4e8f8d9e0ebab382
Luke Carl Hartman <luke@lukecarlhartman.com>
2024-04-29 11:30:46 -0700

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: 'selector',
+  theme: {
+    extend: {},
+  },
+  content: [
+    // Example content paths...
+    './site/templates/**.php',
+    './site/snippets/**.php',
+    './assets/js/**.js'
+  ],
+  variants: {},
+  plugins: [],
+}
